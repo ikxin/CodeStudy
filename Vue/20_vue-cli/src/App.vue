a76e1faccd2b0809cb875682e38045cd7994cd2c
一纸忘忧 <i@ikxin.com>
2022-05-17 11:38:12 +0800

    默认插槽

--- Vue/20_vue-cli/src/App.vue
+++ Vue/20_vue-cli/src/App.vue
@@ -1,15 +1,39 @@
 <template>
   <div class="container">
-    <Search />
-    <List />
+    <Category title="美食" :listData="foods">
+      <img src="./assets/logo.png" />
+    </Category>
+    <Category title="游戏">
+      <ul>
+        <li v-for="(item, index) in games" :key="index">{{ item }}</li>
+      </ul>
+    </Category>
+    <Category title="电影">
+      <ul>
+        <li v-for="(item, index) in films" :key="index">{{ item }}</li>
+      </ul>
+    </Category>
   </div>
 </template>
 
 <script>
-import Search from './components/Search.vue'
-import List from './components/List.vue'
+import Category from './components/Category.vue'
 export default {
   name: 'App',
-  components: { Search, List }
+  components: { Category },
+  data () {
+    return {
+      foods: ['火锅', '烧烤', '小龙虾', '牛排'],
+      games: ['穿越火线', '王者荣耀', 'QQ飞车', '和平精英'],
+      films: ['《功夫》', '《复仇者联盟》', '《钢铁侠》', '《流浪地球》']
+    }
+  }
 }
 </script>
+
+<style scoped>
+.container {
+  display: flex;
+  justify-content: space-around;
+}
+</style>
---
+++ Vue/20_vue-cli/src/components/Category.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="category">
+    <h2>{{ title }}分类</h2>
+    <!-- 定义一个插槽 -->
+    <slot>默认值</slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CategoryView',
+  props: ['title']
+}
+</script>
+
+<style scoped>
+.category {
+  background-color: skyblue;
+  width: 200px;
+  height: 300px;
+}
+h2 {
+  text-align: center;
+  background-color: orange;
+}
+img {
+  width: 100%;
+}
+</style>
