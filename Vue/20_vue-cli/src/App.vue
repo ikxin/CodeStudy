aa28a23112d577d68c58c4e49c963ddc4802b40a
一纸忘忧 <i@ikxin.com>
2022-05-19 11:18:16 +0800

    ElementUI基本使用

--- Vue/20_vue-cli/src/App.vue
+++ Vue/20_vue-cli/src/App.vue
@@ -1,5 +1,18 @@
 <template>
-  <div></div>
+  <div>
+    <el-card>
+      <button>原生按钮</button>
+      <input type="text" />
+    </el-card>
+    <el-card>
+      <el-button>默认按钮</el-button>
+      <el-button type="primary">主要按钮</el-button>
+      <el-button type="success">成功按钮</el-button>
+    </el-card>
+    <el-card>
+      <el-date-picker type="date" placeholder="选择日期"> </el-date-picker>
+    </el-card>
+  </div>
 </template>
 
 <script>
--- Vue/20_vue-cli/src/main.js
+++ Vue/20_vue-cli/src/main.js
@@ -2,9 +2,15 @@
 import Vue from 'vue'
 // 引入App组件，它是所有组件的父组件
 import App from './App.vue'
+// 引入ElementUI组件库
+import ElementUI from 'element-ui'
+// 引入ElementUI全部样式
+import 'element-ui/lib/theme-chalk/index.css'
 
 // 阻止 vue 在启动时生成生产提示
 Vue.config.productionTip = false
+
+Vue.use(ElementUI)
 
 // 创建Vue实例对象
 new Vue({
