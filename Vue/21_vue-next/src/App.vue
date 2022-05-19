8f71a485351c43c3bd5ec5bcdc5bc37d3e11e0e6
一纸忘忧 <i@ikxin.com>
2022-05-19 15:33:07 +0800

    ref函数处理基本类型

--- Vue/21_vue-next/src/App.vue
+++ Vue/21_vue-next/src/App.vue
@@ -2,41 +2,30 @@
   <h2>我是App组件</h2>
   <h2>姓名：{{ name }}</h2>
   <h2>年龄：{{ age }}</h2>
-  <hr>
-  <h2>性别：{{ sex }}</h2>
-  <button @click="sayHello">弹窗</button>
+  <button @click="changeInfo">更改信息</button>
 </template>
 
 <script>
-// import { h } from 'vue'
+import { ref } from 'vue'
 
 export default {
   name: 'App',
   setup() {
     // 数据
-    let name = '张三'
-    let age = 18
+    let name = ref('张三')
+    let age = ref(18)
 
     // 方法
-    function sayHello() {
-      alert(`姓名：${name}，年龄：${age}`)
+    function changeInfo() {
+      name.value = '李四'
+      age.value = 20
     }
 
     // 返回一个对象
     return {
       name,
       age,
-      sayHello
-    }
-
-    // 返回一个函数（渲染函数）
-    // return () => {
-    //   return h('h2', '一纸忘忧')
-    // }
-  },
-  data() {
-    return {
-      sex: '男'
+      changeInfo
     }
   }
 }
