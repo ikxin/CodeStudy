19f34c8c3165b54a3ee077872415ea4d15368a74
一纸忘忧 <i@ikxin.com>
2022-05-19 11:04:07 +0800

    Vue路由

--- Vue/20_vue-cli/23_Vue路由/main.js
+++ Vue/20_vue-cli/23_Vue路由/main.js
@@ -2,9 +2,15 @@
 import Vue from 'vue'
 // 引入App组件，它是所有组件的父组件
 import App from './App.vue'
+// 引入VueRouter
+import VueRouter from 'vue-router'
+// 引入路由器
+import router from './router'
 
 // 阻止 vue 在启动时生成生产提示
 Vue.config.productionTip = false
+
+Vue.use(VueRouter)
 
 // 创建Vue实例对象
 new Vue({
@@ -12,5 +18,6 @@ new Vue({
   render: h => h(App),
   beforeCreate () {
     Vue.prototype.$bus = this
-  }
+  },
+  router
 })
